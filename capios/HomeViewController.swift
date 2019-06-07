ae4ffdac3540e759f476fbcf0f7af45252805191
Francisco Costa Neto <costaneto222@gmai.com>
2019-06-07 09:13:54 -0300

    Inicio do desenvolvimento da atividade de Keychain e Acessibilidade

--- capios/HomeViewController.swift
+++ capios/HomeViewController.swift
@@ -28,6 +28,7 @@ class HomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.title = "App Exercícios"
     }
     
 
